a6df3abaa91ee2dfefcdf896ba6bfbeaa3b49ea1
ArtemSench <79895197815@ya.ru>
2024-10-22 16:15:51 +0300

    Module_19_4

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Buyer)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('cost', 'description',)
+    fieldsets = (
+        ('info',{
+            'fields':
+                ('title', 'cost', 'size')
+        }),
+        ('footer',{
+            'fields':
+                ('description', 'age_limited')
+        }),
+    )
